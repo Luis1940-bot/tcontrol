7241943375b2c474a0c4c75074b3531308a117b2
Luis Gimenez <luisconsultor@outlook.com>
2025-03-18 09:08:02 -0300

    primer update

--- index.php
+++ index.php
@@ -1,45 +1,54 @@
 <?php
-// session_start([
-//     'cookie_secure' => true, // Asegura que la cookie solo se envía sobre HTTPS 
-//     'cookie_httponly' => true, // Evita el acceso de JavaScript a la cookie 
-//     'cookie_samesite' => 'Strict' // Previene ataques CSRF
-// ]);
 
 header('Content-Type: text/html;charset=utf-8');
 $nonce = base64_encode(random_bytes(16));
 header("Content-Security-Policy: default-src 'self'; img-src 'self' data: https: example.com; script-src 'self' 'nonce-$nonce' cdn.example.com; style-src 'self' 'nonce-$nonce' cdn.example.com; object-src 'none'; base-uri 'self'; form-action 'self'; upgrade-insecure-requests;");
 
-header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload"); 
-header("X-Content-Type-Options: nosniff"); 
-header("X-Frame-Options: DENY"); 
+header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload");
+header("X-Content-Type-Options: nosniff");
+header("X-Frame-Options: DENY");
 header("X-XSS-Protection: 1; mode=block");
 
-header("Access-Control-Allow-Origin: https://tenkiweb.com"); 
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE"); 
-header("Access-Control-Allow-Headers: Content-Type, Authorization"); 
-header("Access-Control-Allow-Credentials: true"); 
+header("Access-Control-Allow-Origin: https://tenkiweb.com");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
 
 $url = "https://tenkiweb.com/tcontrol/index.php";
 
-if (isset($_SESSION['login_sso'])) {
-    define('SSO', $_SESSION['login_sso']['sso'] ?? null);
-    define('EMAIL', $_SESSION['login_sso']['email'] ?? null);
+
+if (isset($_SESSION['login_sso']) && is_array($_SESSION['login_sso'])) {
+  /** @var array{email: string, sso: string} $login_sso */
+  $login_sso = $_SESSION['login_sso'];
+
+  define('SSO', $login_sso['sso']);
+  define('EMAIL', $login_sso['email']);
 } else {
-    define('SSO', null);
-    define('EMAIL', null);
+  define('SSO', null);
+  define('EMAIL', null);
+  $login_sso = null;
 }
 
-if (!isset($_SESSION['login_sso']['email']) && (SSO === null || SSO === 's_sso')) {
-    $url = "https://tenkiweb.com/tcontrol/Pages/Login/index.php";
-    // header("Location: ". $url ."");
+if (!is_array($login_sso) || (SSO === null || SSO === 's_sso')) {
+  $url = "https://tenkiweb.com/tcontrol/Pages/Login/index.php";
+  // header("Location: ". $url ."");
 }
+
+
+require_once './config.php';
 require_once __DIR__ . '/ErrorLogger.php';
 ErrorLogger::initialize(__DIR__ . '/logs/error.log');
-require_once './config.php';
+
+/** @var string $baseUrl */
+$baseUrl = BASE_URL;
+/** @var string $emailUrl */
+$emailUrl = EMAIL;
+
 
 ?>
 <!DOCTYPE html>
 <html lang='es'>
+
 <head>
   <meta charset='UTF-8'>
   <meta http-equiv='X-UA-Compatible' content='IE=edge'>
@@ -47,23 +56,35 @@ require_once './config.php';
   <meta name='description' content=''>
   <meta name='author' content='Luis1940-bot'>
 
-  <link rel='shortcut icon' type = 'image / x-icon' href='<?php echo BASE_URL ?>/assets/img/favicon.ico'>
-  <link rel='stylesheet' type='text/css' href='<?php echo BASE_URL ?>/assets/css/style.css?v=<?php echo(time()); ?>' media='screen'>
-  <link rel='stylesheet' type='text/css' href='<?php echo BASE_URL ?>/assets/css/spinner.css?v=<?php echo(time()); ?>' media='screen'>
+  <link rel='shortcut icon' type='image / x-icon' href='<?php echo $baseUrl ?>/assets/img/favicon.ico'>
+  <link rel='stylesheet' type='text/css' href='<?php echo $baseUrl ?>/assets/css/style.css?v=<?php echo (time()); ?>' media='screen'>
+  <link rel='stylesheet' type='text/css' href='<?php echo $baseUrl ?>/assets/css/spinner.css?v=<?php echo (time()); ?>' media='screen'>
   <title></title>
-  <script nonce="<?= $nonce ?>"> // Código JavaScript seguro </script> 
-  <style nonce="<?= $nonce ?>"> /* Código CSS seguro */ </style>
+  <script src="<?= BASE_URL ?>/assets/js/disableConsole.js"></script>
+
+
+
+
+  <script nonce="<?= $nonce ?>">
+    // Código JavaScript seguro 
+  </script>
+  <style nonce="<?= $nonce ?>">
+    /* Código CSS seguro */
+  </style>
 </head>
+
 <body>
-  <input type="hidden" id="email" value="<?php echo EMAIL; ?>">
+  <input type="hidden" id="email" value="<?php echo $emailUrl; ?>">
   <div class="spinner"></div>
   <div class='headerFactum'>
-      <div class='logoFactum'>
-        <a id='linkInstitucional'>
-          <img id='logo_png'>
-        </a>
-        </div>
+    <div class='logoFactum'>
+      <a id='linkInstitucional'>
+        <img id='logo_png'>
+      </a>
     </div>
-<script type='module' src='<?php echo BASE_URL ?>/controllers/index.js?v=<?php echo(time()); ?>'></script>
+  </div>
+  <script type='module' src='<?php echo $baseUrl ?>/controllers/index.js?v=<?php echo (time()); ?>'></script>
+
 </body>
+
 </html>
