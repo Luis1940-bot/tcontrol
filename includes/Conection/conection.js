f3fdfffd4d3ba4eca949a3a6a939df2cea18320e
Luis Gimenez <luisconsultor@outlook.com>
2025-03-18 09:57:39 -0300

    primer update

--- includes/Conection/conection.js
+++ includes/Conection/conection.js
@@ -1,32 +1,33 @@
-import baseUrl from '../../config.js'
-const SERVER = baseUrl
+import baseUrl from '../../config.js';
+
+const SERVER = baseUrl;
 
 function inicioPerformance() {
-  const inicio = performance.now()
-  sessionStorage.setItem('performance', inicio)
+  const inicio = performance.now();
+  sessionStorage.setItem('performance', inicio);
 }
 function finPerformance() {
-  const fin = performance.now()
-  const inicio = sessionStorage.getItem('performance')
-  const velocidadMbps = fin - inicio
-  const signal = document.getElementById('idSignal')
+  const fin = performance.now();
+  const inicio = sessionStorage.getItem('performance');
+  const velocidadMbps = fin - inicio;
+  const signal = document.getElementById('idSignal');
   // signal.style.height = '12px';
   // signal.style.width = '12px';
-  const ruta = `${SERVER}/assets/img/`
+  const ruta = `${SERVER}/assets/img/`;
   if (velocidadMbps > 5) {
-    signal.src = `${ruta}caracol.png`
-    signal.alt = 'No connetc'
+    signal.src = `${ruta}caracol.png`;
+    signal.alt = 'No connetc';
     // console.log('mala conexion');
   } else if (velocidadMbps > 2 && velocidadMbps <= 5) {
-    signal.src = `${ruta}liebre.png`
-    signal.alt = 'Lost connetc'
+    signal.src = `${ruta}liebre.png`;
+    signal.alt = 'Lost connetc';
     // console.log('conexión intermedia');
   } else {
-    signal.src = `${ruta}liebre.png`
-    signal.alt = 'Good connetc'
+    signal.src = `${ruta}liebre.png`;
+    signal.alt = 'Good connetc';
     // console.log('buena conexión');
   }
 }
 
 // Llama a la función al cargar la página para mostrar el estado inicial
-export { inicioPerformance, finPerformance }
+export { inicioPerformance, finPerformance };
