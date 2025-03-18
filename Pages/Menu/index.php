d4c3eeab42e1e05ae59ba601a6de38c166905480
Luis Gimenez <luisconsultor@outlook.com>
2025-03-18 11:48:34 -0300

    primer update

--- Pages/Menu/index.php
+++ Pages/Menu/index.php
@@ -1,55 +1,92 @@
 <?php
-if (session_status() == PHP_SESSION_NONE) { session_start(); };
+// ini_set('display_errors', '1');
+// ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL);
+if (session_status() == PHP_SESSION_NONE) {
+  session_start();
+};
 
 header('Content-Type: text/html;charset=utf-8');
 $nonce = base64_encode(random_bytes(16));
 header("Content-Security-Policy: default-src 'self'; img-src 'self' data: https: example.com; script-src 'self' 'nonce-$nonce' cdn.example.com; style-src 'self' 'nonce-$nonce' cdn.example.com; object-src 'none'; base-uri 'self'; form-action 'self'; upgrade-insecure-requests;");
 
-header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload"); 
-header("X-Content-Type-Options: nosniff"); 
-header("X-Frame-Options: DENY"); 
+header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload");
+header("X-Content-Type-Options: nosniff");
+header("X-Frame-Options: DENY");
 header("X-XSS-Protection: 1; mode=block");
 
-header("Access-Control-Allow-Origin: https://tenkiweb.com"); 
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE"); 
-header("Access-Control-Allow-Headers: Content-Type, Authorization"); 
-header("Access-Control-Allow-Credentials: true"); 
+header("Access-Control-Allow-Origin: https://tenkiweb.com");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
 
-  require_once dirname(dirname(__DIR__)) . '/ErrorLogger.php';
-  ErrorLogger::initialize(dirname(dirname(__DIR__)) . '/logs/error.log');
-  require_once dirname(dirname(__DIR__)) . '/config.php';
+require_once dirname(dirname(__DIR__)) . '/ErrorLogger.php';
+ErrorLogger::initialize(dirname(dirname(__DIR__)) . '/logs/error.log');
+require_once dirname(dirname(__DIR__)) . '/config.php';
+/** @var string $baseUrl */
+$baseUrl = BASE_URL;
 
+//**************************************************** */
 // Tiempo de inactividad en segundos (12 horas)
 $inactive = 43200;
-
-// Verifica si la sesión ha estado inactiva por más de 12 horas
-if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $inactive) {
-    session_unset();     // Elimina los datos de sesión
-    session_destroy();   // Destruye la sesión
-    header("Location:  https://tenkiweb.com/tcontrol/index.php"); // Redirige al login o logout
-    exit();
+if (!isset($_SESSION['last_activity']) || $_SESSION['last_activity'] === 0 || !is_int($_SESSION['last_activity'])) {
+  $_SESSION['last_activity'] = time();
 }
-
+$lastActivity = $_SESSION['last_activity'];
+if ((time() - $lastActivity) > $inactive) {
+  session_unset();
+  session_destroy();
+  header("Location: https://tenkiweb.com/tcontrol/index.php");
+  exit();
+}
 // Actualiza la última actividad
 $_SESSION['last_activity'] = time();
+//**************************************************** */
 
-$url = BASE_URL . "/index.php"; //*"https://tenkiweb.com/tcontrol/index.php";
-define('SSO', $_SESSION['login_sso']['sso']);
- if (isset($_SESSION['login_sso']['email'] )) {
-      define('EMAIL', $_SESSION['login_sso']['email']);
-      
+/** 
+ * @var array{
+ *     login_sso?: array{
+ *         sso?: string|null,
+ *         email?: string|null
+ *     }
+ * } $_SESSION 
+ */
+$url = $baseUrl . "/index.php"; //*"https://tenkiweb.com/tcontrol/index.php";
+if (isset($_SESSION['login_sso']) && is_array($_SESSION['login_sso'])) {
+  if (isset($_SESSION['login_sso']['sso'])) {
+    define('SSO', $_SESSION['login_sso']['sso']);
   } else {
-    if ( SSO === null || SSO === 's_sso' ) {
-      $url = BASE_URL . "/Pages/Login/index.php"; //*"https://tenkiweb.com/tcontrol/index.php";
-    }
-    //! ESTO ES NECESARIOCUANDO SE TRABAJA CON SSO
-    header("Location: ". $url ."");
+    define('SSO', null);
   }
 
-  if (isset($_SESSION['timezone'])) {
-    date_default_timezone_set($_SESSION['timezone']);
+  if (isset($_SESSION['login_sso']['email'])) {
+    define('EMAIL', $_SESSION['login_sso']['email']);
+  } else {
+    define('EMAIL', null);
+  }
 } else {
-    date_default_timezone_set('America/Argentina/Buenos_Aires');
+  define('SSO', null);
+  define('EMAIL', null);
+}
+
+if (EMAIL !== null) {
+  // Aquí se mantiene la lógica si hay un email definido
+} else {
+  if (SSO === null || SSO === 's_sso') {
+    $url = $baseUrl . "/Pages/Login/index.php"; //*"https://tenkiweb.com/tcontrol/index.php";
+  }
+
+  header("Location: " . $url);
+  exit();
+}
+
+/** 
+ * @var array{timezone?: string} $_SESSION 
+ */
+if (isset($_SESSION['timezone']) && is_string($_SESSION['timezone'])) {
+  date_default_timezone_set($_SESSION['timezone']);
+} else {
+  date_default_timezone_set('America/Argentina/Buenos_Aires');
 }
 // echo "Zona horaria actual: " . date_default_timezone_get() . "<br>";
 // echo "Fecha y hora actual: " . date('Y-m-d H:i:s') . "<br>";
@@ -57,26 +94,29 @@ define('SSO', $_SESSION['login_sso']['sso']);
 ?>
 <!DOCTYPE html>
 <!-- <html lang='en'> -->
+
 <head>
   <meta charset='UTF-8'>
   <meta name='description'>
   <meta name='author' content='Luis1940-bot'>
   <meta http-equiv='X-UA-Compatible' content='IE=edge'>
   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-  <link rel='shortcut icon' type = 'image / x-icon' href='<?php echo BASE_URL ?>/assets/img/favicon.ico'>
-  <link rel='stylesheet' type='text/css' href='<?php echo BASE_URL ?>/Pages/Menu/menu.css?v=<?php echo(time()); ?>' media='screen'>
-  <link rel='stylesheet' type='text/css' href='<?php echo BASE_URL ?>/assets/css/spinner.css?v=<?php echo(time()); ?>' media='screen'>
+  <link rel='shortcut icon' type='image / x-icon' href='<?php echo BASE_URL ?>/assets/img/favicon.ico'>
+  <link rel='stylesheet' type='text/css' href='<?php echo BASE_URL ?>/Pages/Menu/menu.css?v=<?php echo (time()); ?>' media='screen'>
+  <link rel='stylesheet' type='text/css' href='<?php echo BASE_URL ?>/assets/css/spinner.css?v=<?php echo (time()); ?>' media='screen'>
   <title></title>
+  <script src="<?= BASE_URL ?>/assets/js/disableConsole.js"></script>
 </head>
+
 <body>
   <div class="spinner"></div>
   <header>
-    
+
     <?php
-      include_once('../../includes/molecules/header.php');
-      include_once('../../includes/molecules/encabezado.php');
-      include_once('../../includes/molecules/whereUs.php');
-      
+    include_once('../../includes/molecules/header.php');
+    include_once('../../includes/molecules/encabezado.php');
+    include_once('../../includes/molecules/whereUs.php');
+
     ?>
   </header>
   <main>
@@ -86,10 +126,11 @@ define('SSO', $_SESSION['login_sso']['sso']);
   </main>
   <footer>
     <?php
-      include_once('../../includes/molecules/footer.php');
+    include_once('../../includes/molecules/footer.php');
     ?>
   </footer>
-  <script type='module' src='<?php echo BASE_URL ?>/config.js?v=<?php echo(time()); ?>'></script>
-  <script type='module' src='<?php echo BASE_URL ?>/Pages/Menu/menu.js?v=<?php echo(time()); ?>'></script>
+  <script type='module' src='<?php echo BASE_URL ?>/config.js?v=<?php echo (time()); ?>'></script>
+  <script type='module' src='<?php echo BASE_URL ?>/Pages/Menu/menu.js?v=<?php echo (time()); ?>'></script>
 </body>
+
 </html>
--- Pages/Menu/menu.js
+++ Pages/Menu/menu.js
@@ -1,74 +1,74 @@
 // eslint-disable-next-line no-unused-vars, import/extensions
-import readJSON from '../../controllers/read-JSON.js'
+import readJSON from '../../controllers/read-JSON.js';
 // eslint-disable-next-line import/extensions
-import createButton from '../../includes/atoms/createButton.js'
+import createButton from '../../includes/atoms/createButton.js';
 // eslint-disable-next-line import/extensions
-import personModal from '../../controllers/person.js'
+import personModal from '../../controllers/person.js';
 // eslint-disable-next-line import/extensions
 import {
   inicioPerformance,
   finPerformance,
-} from '../../includes/Conection/conection.js'
+} from '../../includes/Conection/conection.js';
 // eslint-disable-next-line import/extensions, import/no-useless-path-segments
-import { desencriptar } from '../../controllers/cript.js'
+import { desencriptar } from '../../controllers/cript.js';
 
-import baseUrl from '../../config.js'
-import { configPHP } from '../../controllers/configPHP.js'
-import { arraysLoadTranslate } from '../../controllers/arraysLoadTranslate.js'
-import { trO } from '../../controllers/trOA.js'
-import Alerta from '../../includes/atoms/alerta.js'
-import arrayGlobal from '../../controllers/variables.js'
-import LogOut from '../../controllers/logout.js'
+import baseUrl from '../../config.js';
+import { configPHP } from '../../controllers/configPHP.js';
+import { arraysLoadTranslate } from '../../controllers/arraysLoadTranslate.js';
+import { trO } from '../../controllers/trOA.js';
+import Alerta from '../../includes/atoms/alerta.js';
+import arrayGlobal from '../../controllers/variables.js';
+import LogOut from '../../controllers/logout.js';
 
-const SERVER = baseUrl
-let objTranslate = []
+const SERVER = baseUrl;
+let objTranslate = [];
 
-const spinner = document.querySelector('.spinner')
-const objButtons = {}
+const spinner = document.querySelector('.spinner');
+const objButtons = {};
 const navegador = {
   estadoAnteriorButton: '',
   estadoAnteriorWhereUs: [],
-}
+};
 
 function leeVersion(json) {
   readJSON(json)
     .then((data) => {
-      document.querySelector('.version').innerText = data.version
+      document.querySelector('.version').innerText = data.version;
     })
     .catch((error) => {
       // eslint-disable-next-line no-console
       // console.error('Error al cargar el archivo:', error)
-      const miAlerta = new Alerta()
-      const obj = arrayGlobal.avisoRojo
+      const miAlerta = new Alerta();
+      const obj = arrayGlobal.avisoRojo;
       const texto =
         trO('Error al cargar el archivo.', objTranslate) ||
-        'Error al cargar el archivo.'
-      miAlerta.createVerde(obj, texto, objTranslate)
-      const modal = document.getElementById('modalAlertVerde')
-      modal.style.display = 'block'
-    })
+        'Error al cargar el archivo.';
+      miAlerta.createVerde(obj, texto, objTranslate);
+      const modal = document.getElementById('modalAlertVerde');
+      modal.style.display = 'block';
+    });
 }
 
 /* eslint-disable no-use-before-define */
 function asignarEventos() {
-  const buttons = document.querySelectorAll('.button-selector-menu')
+  const buttons = document.querySelectorAll('.button-selector-menu');
   buttons.forEach((button, index) => {
     button.addEventListener('click', () => {
-      const ruta = objButtons[navegador.estadoAnteriorButton].name[index]
-      window.location.href = `${SERVER}/Pages/${ruta}/index.php`
-    })
-  })
+      const ruta = objButtons[navegador.estadoAnteriorButton].name[index];
+      window.location.href = `${SERVER}/Pages/${ruta}/index.php`;
+    });
+  });
 }
 
 /* eslint-enable no-use-before-define */
 
 function completaButtons(obj) {
-  const divButtons = document.querySelector('.div-menu-buttons')
-  divButtons.innerHTML = ''
-  document.getElementById('spanUbicacion').innerText = objButtons.planta
+  const divButtons = document.querySelector('.div-menu-buttons');
+  divButtons.innerHTML = '';
+  document.getElementById('spanUbicacion').innerText = objButtons.planta;
   // eslint-disable-next-line no-plusplus
   for (let i = 0; i < objButtons[obj].name.length; i++) {
-    const element = objButtons[obj].name[i]
+    const element = objButtons[obj].name[i];
     // const ruta = objButtons[obj].ruta[i];
 
     const params = {
@@ -96,109 +96,111 @@ function completaButtons(obj) {
       outi: null,
       tipo: null,
       procedure: null,
-    }
-    const newButton = createButton(params)
-    divButtons.appendChild(newButton)
+    };
+    const newButton = createButton(params);
+    divButtons.appendChild(newButton);
   }
-  asignarEventos()
+  asignarEventos();
 }
 
 function leeApp(json) {
   readJSON(json)
     .then((data) => {
-      Object.assign(objButtons, data)
-      navegador.estadoAnteriorButton = 'Menu'
-      navegador.estadoAnteriorWhereUs.push('Menu')
-      completaButtons('Menu')
+      Object.assign(objButtons, data);
+      navegador.estadoAnteriorButton = 'Menu';
+      navegador.estadoAnteriorWhereUs.push('Menu');
+      completaButtons('Menu');
     })
     .catch((error) => {
       // eslint-disable-next-line no-console
-      console.error('Error al cargar el archivo:', error)
-    })
+      console.error('Error al cargar el archivo:', error);
+    });
 }
 
 function dondeEstaEn() {
   // const ustedEstaEn = `${trO('Usted está en')} ` || 'Usted está en ';
   // document.getElementById('whereUs').innerText = ustedEstaEn;
-  let lugar = trO('Menú', objTranslate) || 'Menú'
-  lugar = `<img src='${SERVER}/assets/img/icons8-brick-wall-50.png' height='10px' width='10px'> ${lugar}`
-  document.getElementById('whereUs').innerHTML = lugar
-  document.getElementById('whereUs').style.display = 'inline'
-  document.getElementById('volver').style.display = 'block'
+  let lugar = trO('Menú', objTranslate) || 'Menú';
+  lugar = `<img src='${SERVER}/assets/img/icons8-brick-wall-50.png' height='10px' width='10px'> ${lugar}`;
+  document.getElementById('whereUs').innerHTML = lugar;
+  document.getElementById('whereUs').style.display = 'inline';
+  document.getElementById('volver').style.display = 'block';
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const user = desencriptar(sessionStorage.getItem('user'))
-  const { plant } = user
+  const user = desencriptar(sessionStorage.getItem('user'));
+  const { plant } = user;
 
-  inicioPerformance()
-  configPHP(user, SERVER)
-  document.querySelector('.header-McCain').style.display = 'none'
-  spinner.style.visibility = 'visible'
+  inicioPerformance();
+  configPHP(user, SERVER);
+  document.querySelector('.header-McCain').style.display = 'none';
+  spinner.style.visibility = 'visible';
 
-  const hamburguesa = document.querySelector('#hamburguesa')
-  hamburguesa.style.display = 'none'
+  const hamburguesa = document.querySelector('#hamburguesa');
+  hamburguesa.style.display = 'none';
 
-  const persona = desencriptar(sessionStorage.getItem('user'))
+  const persona = desencriptar(sessionStorage.getItem('user'));
   if (persona) {
     document.querySelector('.custom-button').innerText =
-      persona.lng.toUpperCase()
-    await leeVersion('version')
+      persona.lng.toUpperCase();
+    await leeVersion('version');
 
     async function iniciarAplicacion() {
-      objTranslate = await arraysLoadTranslate()
-      dondeEstaEn()
-      leeApp(`App/${plant}/app`)
-      spinner.style.visibility = 'hidden'
-      finPerformance()
+      objTranslate = await arraysLoadTranslate();
+      dondeEstaEn();
+      leeApp(`App/${plant}/app`);
+      spinner.style.visibility = 'hidden';
+      finPerformance();
     }
 
-    requestAnimationFrame(iniciarAplicacion)
+    requestAnimationFrame(iniciarAplicacion);
   } else {
-    spinner.style.visibility = 'hidden'
-    finPerformance()
+    spinner.style.visibility = 'hidden';
+    finPerformance();
   }
-})
+});
 
 document.addEventListener('DOMContentLoaded', () => {
-  const person = document.getElementById('person')
+  const person = document.getElementById('person');
   person.addEventListener('click', () => {
-    person.style.border = '3px solid #212121'
-    person.style.background = '#212121'
-    person.style.borderRadius = '10px 10px 0px 0px'
-    const persona = desencriptar(sessionStorage.getItem('user'))
+    person.style.border = '3px solid #212121';
+    person.style.background = '#212121';
+    person.style.borderRadius = '10px 10px 0px 0px';
+    const persona = desencriptar(sessionStorage.getItem('user'));
+    const quienEs = document.getElementById('spanPerson');
+    quienEs.innerText = persona.person;
     const user = {
       person: persona.person,
       home: 'Inicio',
       salir: trO('Cerrar sesión', objTranslate),
-    }
-    personModal(user, objTranslate)
-  })
-  setTimeout(function () {
-    alert('Tu sesión está por expirar. Haz clic en Aceptar para continuar.')
-    LogOut()
-  }, 43200000 - 300000)
-})
+    };
+    personModal(user, objTranslate);
+  });
+  setTimeout(() => {
+    alert('Tu sesión está por expirar. Haz clic en Aceptar para continuar.');
+    LogOut();
+  }, 43200000 - 300000);
+});
 
-const goLanding = document.querySelector('.custom-button')
+const goLanding = document.querySelector('.custom-button');
 goLanding.addEventListener('click', () => {
-  const url = `${SERVER}/Pages/Landing`
-  window.location.href = url
-})
+  const url = `${SERVER}/Pages/Landing`;
+  window.location.href = url;
+});
 
 function goBack() {
-  const url = `${SERVER}/Pages/Home`
-  window.location.href = url
+  const url = `${SERVER}/Pages/Home`;
+  window.location.href = url;
 }
 
-const volver = document.getElementById('volver')
+const volver = document.getElementById('volver');
 volver.addEventListener('click', () => {
-  goBack()
-})
+  goBack();
+});
 
 document.addEventListener('keydown', (event) => {
   if (event.key === 'Escape') {
-    event.preventDefault()
-    goBack()
+    event.preventDefault();
+    goBack();
   }
-})
+});
